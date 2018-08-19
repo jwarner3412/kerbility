ec1f80d5eb966d7f71818f5f759768d8828ad7b9
James <jwarner3412@gmail.com>
2018-08-19 02:19:04 -0400

    still having src problems

--- assets/scripts/main-head.js
+++ assets/scripts/main-head.js
@@ -3,7 +3,7 @@ window.onload = function() {
   var timeoutID = window.setTimeout(function() {
     var mainScript = document.createElement('script');
     mainScript.setAttribute('type', 'text/javascript');
-    mainScript.setAttribute('src', 'assets/scripts/main-foot.js');
+    mainScript.setAttribute('src', '/kerbility/assets/scripts/main-foot.js');
     document.body.appendChild(mainScript);
   }, 5000)
 }
