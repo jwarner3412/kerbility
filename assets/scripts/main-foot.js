e9356251b250db70780adb30ffd35143c28718e2
James <jwarner3412@gmail.com>
2018-08-19 01:38:06 -0400

    commented out ad code

--- assets/scripts/main-foot.js
+++ assets/scripts/main-foot.js
@@ -1,13 +1,13 @@
 'use strict';
 
-// BidVertiser variables
-var dec_pid = 719128;
-var dec_bid = 1784823;
-// infolinks variables
-var infolinks_pid = 2727670;
-var infolinks_wsid = 0;
-// viglinks key
-var vglnk = { key: '9c1c9c3af2b6bbade7ed3a0281ffae18' };
+// // BidVertiser variables
+// var dec_pid = 719128;
+// var dec_bid = 1784823;
+// // infolinks variables
+// var infolinks_pid = 2727670;
+// var infolinks_wsid = 0;
+// // viglinks key
+// var vglnk = { key: '9c1c9c3af2b6bbade7ed3a0281ffae18' };
 
 // 5 sec timout inserts ads after pageload for google results
 (function () {
@@ -47,30 +47,30 @@ var vglnk = { key: '9c1c9c3af2b6bbade7ed3a0281ffae18' };
   ga('create', 'UA-76237224-1', 'auto');
   ga('send', 'pageview');
 
-// select is the jquery selector where the ad goes, url is script src
-  var adList = [
-    {select: '.revhits-square1', url: '//clksite.com/adServe/banners?tid=113089_215589_0'},
-    {select: '.revhits-square2', url: '//clksite.com/adServe/banners?tid=113089_215589_1'},
-    {select: '.revhits-square3', url: '//clksite.com/adServe/banners?tid=113089_215589_4'},
-    {select: '.revhits-square4', url: '//clksite.com/adServe/banners?tid=113089_215589_5'},
-    {select: 'body', url: '//resources.infolinks.com/js/infolinks_main.js',},
-    {select: 'body', url: '//clksite.com/adServe/banners?tid=113089_215589_2&type=footer&size=728x90',},
-    {select: 'body', url: '//clksite.com/adServe/banners?tid=113089_215589_3&type=shadowbox&size=300x250'}
-  ];
+// // select is the jquery selector where the ad goes, url is script src
+//   var adList = [
+//     {select: '.revhits-square1', url: '//clksite.com/adServe/banners?tid=113089_215589_0'},
+//     {select: '.revhits-square2', url: '//clksite.com/adServe/banners?tid=113089_215589_1'},
+//     {select: '.revhits-square3', url: '//clksite.com/adServe/banners?tid=113089_215589_4'},
+//     {select: '.revhits-square4', url: '//clksite.com/adServe/banners?tid=113089_215589_5'},
+//     {select: 'body', url: '//resources.infolinks.com/js/infolinks_main.js',},
+//     {select: 'body', url: '//clksite.com/adServe/banners?tid=113089_215589_2&type=footer&size=728x90',},
+//     {select: 'body', url: '//clksite.com/adServe/banners?tid=113089_215589_3&type=shadowbox&size=300x250'}
+//   ];
 
-  function adInject(obj) {
-    $(obj.select).append($(document.createElement('script'))
-      .attr({src: obj.url, type: 'text/javascript'})
-    )
-  }
+//   function adInject(obj) {
+//     $(obj.select).append($(document.createElement('script'))
+//       .attr({src: obj.url, type: 'text/javascript'})
+//     )
+//   }
 
-  adList.forEach(adInject);
+//   adList.forEach(adInject);
 
 
-// viglinks
-  (function(d, t) {
-    var s = d.createElement(t); s.type = 'text/javascript'; s.async = true;
-    s.src = '//cdn.viglink.com/api/vglnk.js';
-    var r = d.getElementsByTagName(t)[0]; r.parentNode.insertBefore(s, r);
-  }(document, 'script'));
-})();
+// // viglinks
+//   (function(d, t) {
+//     var s = d.createElement(t); s.type = 'text/javascript'; s.async = true;
+//     s.src = '//cdn.viglink.com/api/vglnk.js';
+//     var r = d.getElementsByTagName(t)[0]; r.parentNode.insertBefore(s, r);
+//   }(document, 'script'));
+// })();
